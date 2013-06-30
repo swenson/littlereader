31418330c10577136c71bceef62285111382fe5b
Christopher Swenson <chris@caswenson.com>
2013-06-30 12:58:16 -0700

    Add state save, RSS updater

--- src/littlereader/reader.go
+++ src/littlereader/reader.go
@@ -5,10 +5,13 @@ import (
 	"fmt"
 	"github.com/hoisie/web"
 	"io/ioutil"
+	"net/http"
 	"os"
+	"time"
 )
 
 var folders map[string][]*Source
+var dirty = false
 
 func readState() {
 	state := State{}
@@ -70,6 +73,7 @@ function hide(s, link) {
 }
 
 func markAsRead(ctx *web.Context) {
+	dirty = true
 	link := ctx.Params["href"]
 	fmt.Printf("Marking %s as read\n", link)
 	for _, folder := range folders {
@@ -83,8 +87,89 @@ func markAsRead(ctx *web.Context) {
 	}
 }
 
+func saver(ticker *time.Ticker) {
+	for {
+		select {
+		case <-ticker.C:
+			println("Writing state")
+			if dirty {
+				dirty = false
+				state := State{folders}
+
+				bytes, err := json.Marshal(state)
+				if err != nil {
+					panic(err)
+				}
+				err = ioutil.WriteFile("state.json", bytes, 0644)
+				if err != nil {
+					panic(err)
+				}
+			}
+		}
+	}
+}
+
+func updater(ticker *time.Ticker) {
+	for {
+		select {
+		case <-ticker.C:
+			println("Updating feeds")
+
+			for _, folder := range folders {
+				for _, source := range folder {
+					now := time.Now()
+					resp, err := http.Get(source.Url)
+
+					if err != nil {
+						fmt.Printf("Error: %s\n", err.Error())
+						continue
+					}
+					defer resp.Body.Close()
+					body, err := ioutil.ReadAll(resp.Body)
+					if err != nil {
+						fmt.Printf("Error: %s\n", err.Error())
+						continue
+					}
+					newSource, err := readAtom(now, body)
+					if err != nil {
+						newSource, err = readRss(now, body)
+						if err != nil {
+							fmt.Printf("Could not parse as atom or RSS... skipping\n")
+							continue
+						}
+					}
+					updateSource(source, newSource)
+				}
+			}
+			dirty = true
+		}
+	}
+}
+
+func updateSource(source *Source, newSource *Source) {
+	for _, newEntry := range newSource.Entries {
+		var exists = false
+		for _, entry := range source.Entries {
+			if entry.Url == newEntry.Url {
+				exists = true
+				break
+			}
+		}
+		if !exists {
+			source.Entries = append(source.Entries, newEntry)
+		}
+	}
+}
+
 func Reader() {
 	readState()
+
+	saveTicker := time.NewTicker(1 * time.Minute)
+	go saver(saveTicker)
+
+	updateTicker := time.NewTicker(12 * time.Hour)
+	go updater(updateTicker)
+
 	web.Get("/", index)
 	web.Post("/markAsRead", markAsRead)
 	web.Run("0.0.0.0:9090")
